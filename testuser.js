1718de49434bba618453c98bb59e1bd30093cf38
Craig <142868948+CraigTheSpaceBum@users.noreply.github.com>
2025-05-10 23:39:00 -0400

    Add files via upload

--- testuser.js
+++ testuser.js
@@ -12,10 +12,10 @@
       followscount: "",
 
       friends: [
-        { username:"npub282421", displayName:"Jane Doe", status:"Online", statusMessage: "Small Things - Blink 182", profilePic:"https://image.nostr.build/d6660927e5c0747557d87c6ea015f8ac01d12fe659e2681a02797f1ffa7bd8b1.jpg" },
+        { username:"npub282421", displayName:"Voodoo", status:"Online", statusMessage: "Small Things - Blink 182", profilePic:"https://image.nostr.build/d6660927e5c0747557d87c6ea015f8ac01d12fe659e2681a02797f1ffa7bd8b1.jpg" },
         { username:"npub1232232", displayName:"CraigTheSpaceBum", status:"Online", statusMessage: "Work goddamn it", profilePic:"https://bitcoin-cats.ca/Craig%20Head2.png" },
         { username:"npub1622234", displayName:"Jack Dorsey", status:"Online", statusMessage: "", profilePic:"https://imageio.forbes.com/specials-images/imageserve/681512de8fa115416db627be/Jack-Dorsey/0x0.jpg" },
-        { username:"npub1223233", displayName:"Bob Smith", status:"Online", statusMessage: "", profilePic:"https://bitcoin-cats.ca/nostr/senpaicat.png" }
+        { username:"npub1223233", displayName:"Toby", status:"Online", statusMessage: "", profilePic:"https://scontent.fyzd1-3.fna.fbcdn.net/v/t39.30808-6/494197393_10233471802109728_4318288421077047275_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=OSHRdOO2jxEQ7kNvwGdNxCD&_nc_oc=Adnqa1O4cAZvG8bXLy22GhH1r0tcW_MDKgZgyn3ibmiX1lYPCeSR6nBbhnNTCfSe3WTcQiZZoJHv3Vtz0sw8a_Lc&_nc_zt=23&_nc_ht=scontent.fyzd1-3.fna&_nc_gid=VVHb-68_9XnH5C_2WJHwfg&oh=00_AfI6JKnD3z5_S18_BnwtbDTtnJL5TUlWJ4JWA5kq5MWoAw&oe=6825F6F1" }
       ]
     };
 
